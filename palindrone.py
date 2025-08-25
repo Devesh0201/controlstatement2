2d69e8812ece6e2d62ca1dd2d2480914f5403b66
Devesh0201 <devesh66chauhan@gmail.com>
2025-08-25 11:20:22 +0530

    Update palindrone.py

--- palindrone.py
+++ palindrone.py
@@ -1,10 +1,1 @@
-num = int(input("enter the num you want to check"))
-rev=0
 
-for i in range(num):
-    if(num>0):
-        temp = num%10
-        rev = rev*10+temp
-        num = num//10
-    if(num==rev):
-            print("yes")
